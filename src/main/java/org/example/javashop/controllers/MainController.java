9ad940ef4a40e83e111965cbf741218c75703d38
lRaxSonl <malenkijh@gmail.com>
2024-05-21 15:17:54 +0300

    commit

--- src/main/java/org/example/javashop/controllers/MainController.java
+++ src/main/java/org/example/javashop/controllers/MainController.java
@@ -3,6 +3,7 @@ package org.example.javashop.controllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import org.example.javashop.models.User;
@@ -16,16 +17,25 @@ public class MainController {
     private URL location;
 
     @FXML
-    private ListView<String> main_listview;
+    private Button admin_panel_L;
+
+    @FXML
+    private Button products_L;
+
+    @FXML
+    private Button profile_L;
 
     @FXML
     private Label username1;
 
     @FXML
     void initialize() {
-        main_listview.getItems().addAll("Профиль", "Товары", "Админ панель");
         User user = new LoginController().getUser();
-
         username1.setText(user.getUsername());
+
+        profile_L.setOnAction(event -> {
+            System.out.println(user.toString());
+        });
+
     }
 }
